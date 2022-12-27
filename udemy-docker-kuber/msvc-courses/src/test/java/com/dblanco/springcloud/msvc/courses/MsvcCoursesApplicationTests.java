13bc57dd7faa1d655c98f89b76e6eceb130e9897
Daniel <djblancoh@gmail.com>
2022-12-26 21:40:32 -0500

    implement new odule courses, update poms

---
+++ udemy-docker-kuber/msvc-courses/src/test/java/com/dblanco/springcloud/msvc/courses/MsvcCoursesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dblanco.springcloud.msvc.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCoursesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
