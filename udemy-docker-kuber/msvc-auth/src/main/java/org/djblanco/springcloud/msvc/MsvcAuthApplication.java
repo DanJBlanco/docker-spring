cd6f603bf9698909bc8e78cd64f2faac1d5d20cc
Daniel <djblancoh@gmail.com>
2023-01-13 13:30:00 -0500

    create msvc auth

---
+++ udemy-docker-kuber/msvc-auth/src/main/java/org/djblanco/springcloud/msvc/MsvcAuthApplication.java
@@ -0,0 +1,16 @@
+package org.djblanco.springcloud.msvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsvcAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcAuthApplication.class, args);
+    }
+
+}
---
+++ udemy-docker-kuber/msvc-auth/src/test/java/org/djblanco/springcloud/msvc/MsvcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.djblanco.springcloud.msvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
